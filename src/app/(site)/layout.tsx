21f93221050059b7264f721060b033446f8591cb
ClouQuaz <cloudquaz@gmail.com>
2026-06-21 21:38:15 +0530

    cq web files

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,23 @@
+// app/layout.tsx
+
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import CtaSection from '@/components/layout/CtaSection';
+
+export const metadata = {
+    title: 'Exsit Next',
+    description: 'Modern Next.js app with Tailwind + TypeScript + SCSS',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className='font-dm-sans'>
+            <Header />
+            {children}
+            <CtaSection />
+            <Footer />
+
+        </div>
+
+    );
+}
